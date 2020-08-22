ba99707398b8a1e52c838c3a95cb32438c14ede7
Uziel <uzielmelitonh@gmail.com>
2020-08-22 13:22:43 -0500

    Primer commit

---
+++ server/server.js
@@ -0,0 +1,40 @@
+require('./config/config');
+
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+ 
+// parse application/json
+app.use(bodyParser.json())
+
+app.get('/usuario', (req, res) => {    
+    res.json( "get usuario" )
+})
+
+app.post('/usuario', (req, res) => {  
+    const body = req.body;
+    if(body.nombre === undefined){
+        res.status(400).json({
+            status: false,
+            message: "Bad params"
+        });
+    }else{
+        res.json( body  )    
+    }
+})
+
+app.put('/usuario/:id', (req, res) => {    
+    const id = req.params.id;
+    res.json( { id })
+})
+
+app.delete('/usuario', (req, res) => {    
+    res.json( "delete usuario" )
+})
+
+app.listen(3000, () => {
+    console.log("Escuchando en el puerto", process.env.PORT );
+})
